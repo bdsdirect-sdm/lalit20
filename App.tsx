745c073f663aee417d18e7c9c9bf7d219ffea6f1
bdsdirect-sdm <bdsdirect.sdm@smartdatainc.net>
2024-10-11 14:03:42 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default App;
